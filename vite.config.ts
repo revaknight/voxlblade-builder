518996994d63ea0a8320cd80780b37976d8cfdff
Ghao56 <ngmhoangct@gmail.com>
2026-06-01 11:06:41 +0700

    fix github pages base

--- vite.config.ts
+++ vite.config.ts
@@ -3,49 +3,55 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+
+  base:'/voxlblade-builder/',
+
   plugins: [
     svelte(),
 
     VitePWA({
       registerType: 'autoUpdate',
 
-      includeAssets: ['favicon.ico'],
+      includeAssets:['favicon.ico'],
 
       manifest: {
-        name: 'voxlblade builder',
-        short_name: 'voxlbuilder',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
+        name:'voxlblade builder',
+        short_name:'voxlbuilder',
+
+        start_url:'/voxlblade-builder/',
+
+        display:'standalone',
+
+        background_color:'#ffffff',
+        theme_color:'#ffffff',
       },
 
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        globPatterns:['**/*.{js,css,html,ico,png,svg}'],
 
-        skipWaiting: true,
-        clientsClaim: true,
+        skipWaiting:true,
+        clientsClaim:true,
 
-        runtimeCaching: [
+        runtimeCaching:[
           {
-            // HTML (index + route)
-            urlPattern: ({ request }) => request.mode === 'navigate',
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'html-cache',
-            },
-          },
-        ],
-      },
+            urlPattern:({request})=>request.mode==='navigate',
+            handler:'StaleWhileRevalidate',
+
+            options:{
+              cacheName:'html-cache'
+            }
+          }
+        ]
+      }
     }),
   ],
 
-  build: {
-    rollupOptions: {
-      output: {
-        // giảm số request (ít chunk lại)
-        manualChunks: undefined,
-      },
-    },
-  },
+  build:{
+    rollupOptions:{
+      output:{
+        manualChunks:undefined
+      }
+    }
+  }
+
 })
